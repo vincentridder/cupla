6cf875b910807c31ce1f4272ae20d64d593eff23
Ridder <ridder90@hypnos.fz-rossendorf.de>
2016-04-19 17:25:04 +0200

    first ported example:VectorAdd

---
+++ example/CUDASamples/vectorAdd/build/CMakeFiles/alpaka.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ridder90/cupla/alpaka/include/alpaka/core/Dummy.cpp" "/home/ridder90/cupla/example/CUDASamples/vectorAdd/build/CMakeFiles/alpaka.dir/home/ridder90/cupla/alpaka/include/alpaka/core/Dummy.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "ALPAKA_ACC_GPU_CUDA_ENABLED"
+  "ALPAKA_DEBUG=0"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/opt/pkg/devel/boost/1.60.0/gnu/4.9.2/64/opt/include"
+  "/opt/pkg/devel/cuda/7.5/include"
+  "/home/ridder90/cupla/alpaka/include"
+  "../../common"
+  "/home/ridder90/cupla/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
